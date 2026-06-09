8924e1a87df89a802f0a3ccd7dc9b245b5a671a2
Gouri Rajaram <ark.gouri1310@gmail.com>
2026-06-09 13:19:53 -0400

    Update installation.sh

--- hpc_or_local/installation.sh
+++ hpc_or_local/installation.sh
@@ -1,6 +1,6 @@
 #on terminal
 ssh <kerbos_id>@bigpurple.nyumc.org
 module load miniconda3/4.5.1
-pip install dxpy
+pip install --user dxpy
 dx login
 #Enter username and password of your UKB-RAP account
